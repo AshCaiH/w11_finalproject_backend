897348f7bbc572ec69844039c2cd0bd0fbd6df8b
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-03-06 16:23:05 +0000

    feature: request weather at specified location from open-meteo

--- src/common/functions.js
+++ src/common/functions.js
@@ -1,17 +1,53 @@
+const weathercodes = {
+    0: "Clear sky",
+    1: "Mainly clear",
+    2: "Partly cloudy",
+    3: "Overcast",
+
+    45: "Fog",
+    48: "Depositing rime fog",
+
+    51: "Light drizzle",
+    53: "Moderate drizzle",
+    55: "Dense drizzle",
+
+    56: "Light freezing drizzle",
+    57: "Dense freezing drizzle",
+    
+    61: "Slight rain",
+    63: "Moderate rain",
+    65: "Heavy rain",
+
+    66: "Light freezing rain",
+    67: "Heavy freezing rain",
+
+    71: "Slight snow fall",
+    73: "Moderate snow fall",
+    75: "Heavy snow fall",
+
+    77: "Snow grains",
+    
+    80: "Slight rain showers",
+    81: "Moderate rain showers",
+    82: "Violent rain showers",
+    
+    85: "Slight snow showers",
+    86: "Heavy snow showers",
+    
+    95: "Slight or moderate thunderstorm",
+
+    96: "Thunderstorm with slight hail",
+    99: "Thunderstorm with heavy hail",    
+}
+
 module.exports = {
     testRoute: (req, res, next) => {
         res.status(200).json({message: "Success", body: req.body, header: req.headers});
     },
 
-    weatherResponse: (req, res, next) => {
-        res.status(200).json({message: "Weather Query Success", location: req.location});
-    },
-
     requestLocation: async (req, res, next) => {
         try {
             const url = `https://api.geoapify.com/v1/geocode/autocomplete?text=${req.body.location}&apiKey=${process.env.GEOAPIFY_KEY}`
-
-            console.log(url);
 
             const response = await fetch(url, {
                 method: "GET"
@@ -20,7 +56,7 @@ module.exports = {
             req.location = response.features[0].properties;
 
             next();
-        } catch {
+        } catch (error) {
             res.status(500).json({
                 message: error.message,
                 error: error
@@ -30,12 +66,36 @@ module.exports = {
 
     requestWeather: async (req, res, next) => {
         try {
+            const lat = req.location.lat
+            const lon = req.location.lon
+
+            // Note: This doesn't search by date yet, results in current weather and following 6 days.
+
+            const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max&timezone=auto`
+
+            const response = await fetch(url, {
+                method: "GET"
+            }).then((response) => {return response.json()});
+
+            console.log(response);
+
+            req.weather = weathercodes[response.daily.weather_code[0]];
+            req.temperature = response.daily.temperature;
+
             next();
-        } catch {
+        } catch (error) {
             res.status(500).json({
                 message: error.message,
                 error: error
             });
         }
-    }
+    },
+
+    weatherResponse: (req, res, next) => {
+        res.status(200).json({
+            message: "Weather Query Success", 
+            location: req.location,
+            weather: req.weather,
+            "max temperature": req.temperature});
+    },
 }
