39fcab195038d44d940d6ce743915c4cb4c42906
Jaro111 <jarokark@protonmail.com>
2024-03-13 15:39:17 +0000

    hourly chart request

--- src/common/functions.js
+++ src/common/functions.js
@@ -106,27 +106,96 @@ module.exports = {
     try {
       const lat = req.location.lat;
       const lon = req.location.lon;
+      let hourArray = [];
+      let tempArray = [];
+      let rainArray = [];
+      let cloudArray = [];
 
       // Note: This doesn't search by date yet, results in current weather and following 6 days.
+      let newDate = new Date();
+      const today = newDate.toISOString().substring(0, 10);
+      console.log(today);
+
+      let date = new Date();
+      let dayAfter = date.setDate(date.getDate() + 1);
+      dayAfter = date.toISOString().substring(0, 10);
+      console.log(dayAfter);
+
+      const hourNow = date.getHours();
 
       const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max&timezone=auto`;
 
-      const response = await fetch(url, {
-        method: "GET",
-      }).then((response) => {
-        return response.json();
-      });
-      console.log(response);
+      const url2 = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,rain,cloud_cover&timezone=auto&start_date=${today}&end_date=${dayAfter}`;
 
+      const [response1, response2] = await Promise.all([
+        fetch(url, {
+          method: "GET",
+        }).then((response1) => {
+          return response1.json();
+        }),
+        fetch(url2, {
+          method: "GET",
+        }).then((response2) => {
+          return response2.json();
+        }),
+      ]);
+
+      const hours = response2.hourly.time;
+
+      let fullHourArray = [];
+      for (let i = 0; i < hours.length; i++) {
+        let date = new Date(hours[i]);
+        const hour = date.getHours();
+        fullHourArray.push(hour);
+      }
+      const mainIndex = fullHourArray.indexOf(hourNow);
+      for (let i = 0; i < fullHourArray.length; i++) {
+        if (i > mainIndex && hourArray.length < 24) {
+          stringHour = `${fullHourArray[i]}:00`;
+          hourArray.push(stringHour);
+        }
+      }
+      console.log(hourArray);
+
+      //
+      const temps = response2.hourly.temperature_2m;
+      for (let i = 0; i < temps.length; i++) {
+        if (i > mainIndex && tempArray.length < 24) {
+          tempArray.push(temps[i]);
+        }
+      }
+
+      const rain = response2.hourly.rain;
+      console.log(rain);
+      for (let i = 0; i < rain.length; i++) {
+        if (i > mainIndex && rainArray.length < 24) {
+          rainArray.push(rain[i]);
+        }
+      }
+      console.log(rainArray);
+      const clouds = response2.hourly.cloud_cover;
+      for (let i = 0; i < clouds.length; i++) {
+        if (i > mainIndex && cloudArray.length < 24) {
+          cloudArray.push(clouds[i]);
+        }
+      }
       req.weather = {
-        weathername: response.daily.weather_code.map((code) => {
+        weathername: response1.daily.weather_code.map((code) => {
           return weathercodes[code];
         }),
-        weathercode: response.daily.weather_code,
-        temperature: response.daily.temperature_2m_max,
+        weathercode: response1.daily.weather_code,
+        temperature: response1.daily.temperature_2m_max,
+        //
+        hourly_weather: {
+          hourly: hourArray,
+          temperature_2m: tempArray,
+          rain: rainArray,
+          cloud_cover: cloudArray,
+        },
       };
 
       next();
+      //
     } catch (error) {
       res.status(500).json({
         message: error.message,
@@ -139,6 +208,7 @@ module.exports = {
     res.status(200).json({
       message: "Weather Query Success",
       weather: req.weather,
+      hourly_weather: req.hourly_weather,
       location: req.location,
       map: req.mapImage,
     });
